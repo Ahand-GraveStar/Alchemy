867393f1f6124259390f7b3111c913c7d485ef4b
Ahand-GraveStar <rgirla@icloud.com>
2026-01-28 08:31:08 -0600

    Add files via upload

---
+++ Alchemy/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        HerbTypes herbStart = new HerbTypes();
+        herbStart.loadAidHerbs();
+
+        //initialize herbs and potions
+        //do-while loop for main menu
+
+    }
+}
